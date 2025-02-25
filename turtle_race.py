77cde40e86b57fa68544859079169116a6c9b2da
MattiaLR95 <laroccamattia@virgilio.it>
2025-02-25 10:33:41 +0100

    Created the six turtles that will play the race

--- turtle_race.py
+++ turtle_race.py
@@ -4,11 +4,15 @@ from random import randint
 screen = Screen()
 screen.setup(width=500, height=400)
 puntata = screen.textinput(title= "Bet", prompt= "Choose the turtle's color: ")
+colori = ["red", "green", "purple", "yellow", "blue", "magenta"]
+pos_y = [0, -25, -50, 25, 50, 75]
 
-
-t = Turtle("turtle")
-t.color("red")
-t.penup()
-t.goto(x = -240, y = 0)
+turtles = []
+for num in range(6):
+    t = Turtle(shape= "turtle")
+    t.color(colori[num])
+    t.penup()
+    t.goto(x = -240, y = pos_y[num])
+    turtles.append(t)
 
 screen.exitonclick()
